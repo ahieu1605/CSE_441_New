30f3e48b028d34efd09c159189bcae5c32044109
ahieu1605 <thaiphananhhieu2004@gmail.com>
2025-04-15 09:26:29 +0700

    update

--- Project/app/+not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function NotFound() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Trang không tìm thấy!</Text>
-    </View>
-  );
-}
